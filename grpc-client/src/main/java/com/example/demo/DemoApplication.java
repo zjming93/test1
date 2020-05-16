a3277bb9ecec0ff80e756a740a1138c17a9c7a75
zjming93 <2861864745@qq.com>
2020-05-16 15:01:30 +0800

    222

--- grpc-client/src/main/java/com/example/demo/DemoApplication.java
+++ grpc-client/src/main/java/com/example/demo/DemoApplication.java
@@ -9,5 +9,9 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	
+	public static void main2(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
 }
